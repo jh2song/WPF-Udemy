ef44e9ef92fb110ea3dcfa3896243679c404c46b
Jihoon Song <spec0003@naver.com>
2022-06-01 08:27:27 +0900

    ~ 40. The Contacts Class

---
+++ DesktopContactsApp/Classes/Contact.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopContactsApp.Classes
+{
+	class Contact
+	{
+		[PrimaryKey, AutoIncrement]
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public string Phone { get; set; }
+	}
+}
