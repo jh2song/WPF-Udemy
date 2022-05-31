4dde9688f2da01582c1e66a75abe2c61a3044e66
Jihoon Song <spec0003@naver.com>
2022-05-31 16:40:57 +0900

    ~ 37. Working with More Windows

---
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+	/// <summary>
+	/// NewContactWindow.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class NewContactWindow : Window
+	{
+		public NewContactWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
