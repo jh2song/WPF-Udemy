5c3db8e981ecc2dfe990052a91934a24dbdcc988
Jihoon Song <spec0003@naver.com>
2022-06-04 07:40:32 +0900

    ~ 52. Update the SQLite Table

--- DesktopContactsApp/ContactDetailsWindow.xaml.cs
+++ DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -26,11 +26,24 @@ namespace DesktopContactsApp
 			InitializeComponent();
 
 			this.contact = contact;
+			nameTextBox.Text = contact.Name;
+			phoneNumberTextBox.Text = contact.Phone;
+			emailTextBox.Text = contact.Email;
 		}
 
 		private void updateButton_Click(object sender, RoutedEventArgs e)
 		{
+			contact.Name = nameTextBox.Text;
+			contact.Phone = phoneNumberTextBox.Text;
+			contact.Email = emailTextBox.Text;
 
+			using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+			{
+				connection.CreateTable<Contact>();
+				connection.Update(contact);
+			}
+
+			Close();
 		}
 
 		private void deleteButton_Click(object sender, RoutedEventArgs e)
