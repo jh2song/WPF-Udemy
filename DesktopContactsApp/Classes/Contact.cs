007b1b8688a202a149397ec8a4e42e2ad86cc46e
Jihoon Song <spec0003@naver.com>
2022-06-03 14:03:32 +0900

    ~ 46. The ToString method

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -16,5 +16,10 @@ namespace DesktopContactsApp.Classes
 		public string Email { get; set; }
 
 		public string Phone { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Name} - {Email} - {Phone}";
+		}
 	}
 }
