5ffccebbb9ec21b7c36b6d42028edbbbc621849a
Jihoon Song <spec0003@naver.com>
2022-06-03 17:42:47 +0900

    ~ 50. Linq

--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -46,7 +46,11 @@ namespace DesktopContactsApp
 			using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
 			{
 				conn.CreateTable<Contact>();
-				contacts = conn.Table<Contact>().ToList();
+				contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+
+				//var variable = from c2 in contacts
+				//			   orderby c2.Name
+				//			   select c2;
 			}
 
 			if (contacts != null)
@@ -60,6 +64,11 @@ namespace DesktopContactsApp
 			TextBox searchTextBox = sender as TextBox;
 
 			var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+			var filteredList2 = (from c2 in contacts
+								 where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+								 orderby c2.Email
+								 select c2).ToList();
 
 			contactsListView.ItemsSource = filteredList;
 		}
