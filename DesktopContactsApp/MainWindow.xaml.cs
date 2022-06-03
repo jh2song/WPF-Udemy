b6d48dcd527cb89bb3e1bff64fe5482ed677d55d
Jihoon Song <spec0003@naver.com>
2022-06-03 17:15:40 +0900

    ~ 49. Filtering a List

--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -21,9 +21,13 @@ namespace DesktopContactsApp
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		List<Contact> contacts;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			contacts = new List<Contact>();
 
 			ReadDatabase();
 		}
@@ -38,7 +42,7 @@ namespace DesktopContactsApp
 
 		void ReadDatabase()
 		{
-			List<Contact> contacts;
+
 			using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
 			{
 				conn.CreateTable<Contact>();
@@ -49,6 +53,15 @@ namespace DesktopContactsApp
 			{
 				contactsListView.ItemsSource = contacts;
 			}
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			TextBox searchTextBox = sender as TextBox;
+
+			var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+			contactsListView.ItemsSource = filteredList;
 		}
 	}
 }
