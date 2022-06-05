5690a0f791b48457ac8c4bf374080e01ddc010f6
Jihoon Song <spec0003@naver.com>
2022-06-05 14:06:22 +0900

    ~ 62. Selecting a File from the Computer

---
+++ LandmarkAI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandmarkAI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Filter = "Image file (*.png, *.jpg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+			dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+			if (dialog.ShowDialog() == true)
+			{
+				string fileName = dialog.FileName;
+				selectedImage.Source = new BitmapImage(new Uri(fileName));
+			}
+		}
+	}
+}
